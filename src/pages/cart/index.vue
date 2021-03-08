383cf9f7ea83c509cf77bb43c1340cbf25b53558
suxiaoxiao <37326714+gcindy@users.noreply.github.com>
2021-03-08 16:41:23 +0800

    add 购物车title

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -2,11 +2,21 @@
  * @Author: 一个为高薪头秃的程序媴
  * @Date: 2021-03-02 10:59:36
  * @LastEditors: 一个为高薪头秃的程序猿
- * @LastEditTime: 2021-03-02 11:00:35
+ * @LastEditTime: 2021-03-07 22:24:06
  * @Description: 购物车
 -->
 <template>
-  <view class="cart-container">购物车</view>
+  <view class="cart-container">
+    <view class="bgc-f7c659 header-title">
+      <view class="title">购物车</view>
+      <view class="edit">编辑</view>
+    </view>
+    <view class="content-box">
+      <view class="box">111</view>
+      <view class="box">222</view>
+      <view class="box">333</view>
+    </view>
+  </view>
 </template>
 
 <script>
@@ -19,4 +29,6 @@ export default {
   methods: {},
 };
 </script>
-<style lang="less"></style>
+<style lang="less">
+@import url("./index.less");
+</style>
