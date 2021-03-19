e677261cc0aa46f22c95cab32fcd93294c64549b
suxiaoxiao <37326714+gcindy@users.noreply.github.com>
2021-03-19 10:57:46 +0800

    add 封装input&添加地址页面

---
+++ src/components/input/index.vue
@@ -0,0 +1,116 @@
+<!--
+ * @Author: 一个为高薪头秃的程序媴
+ * @Date: 2021-03-18 21:58:58
+ * @LastEditors: 一个为高薪头秃的程序猿
+ * @LastEditTime: 2021-03-19 10:53:48
+ * @Description: 封装input
+-->
+
+<template>
+  <!-- 
+    name       在表单内提交时的标识符
+    label      输入框左侧文本	
+    value      当前输入的值	
+    type       可设置为任意原生类型, 如 number idcard textarea digit	
+    clickable  是否开启点击反馈	
+    clearable	 是否启用清除控件
+    readonly   是否只读	
+    disabled   是否禁用输入框	
+    border     是否显示内边框	
+    required   是否显示表单必填星号
+    focus      是否获取焦点	
+    center     是否使内容垂直居中	
+    password   是否是密码类型	
+    placeholder	输入框为空时占位符
+    placeholder-style	指定 placeholder 的样式	
+
+   -->
+  <van-field
+    :name="name"
+    :label="label"
+    :type="type"
+    :clearable="clearable"
+    :readonly="readonly"
+    :disabled="disabled"
+    :placeholder="placeholder"
+    :border="border"
+    :required="required"
+    :error-message="errorMessage"
+    @change="onChange"
+    @blur="onBlur"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: () => "",
+    },
+    label: {
+      type: String,
+      default: () => "",
+    },
+    value: {
+      type: String,
+      default: () => "",
+    },
+    type: {
+      type: String,
+      default: () => "text",
+    },
+    clearable: {
+      type: Boolean,
+      default: () => true,
+    },
+    readonly: {
+      type: Boolean,
+      default: () => false,
+    },
+    disabled: {
+      type: Boolean,
+      default: () => false,
+    },
+    placeholder: {
+      type: String,
+      default: () => "",
+    },
+    required: {
+      type: Boolean,
+      default: () => false,
+    },
+    border: {
+      type: Boolean,
+      default: () => false,
+    },
+    reg: {
+      type: String,
+      default: () => "",
+    },
+  },
+  data() {
+    return {
+      errorMessage: "",
+    };
+  },
+
+  //方法集合
+  methods: {
+    onChange(e) {
+      const { detail } = e;
+      this.$emit("enterVal", this.name, detail);
+    },
+    onBlur(e) {
+      const { value } = e.detail;
+      if (!value) {
+        this.errorMessage = `请输入${this.name}的信息`;
+      }else {
+        this.errorMessage = "";
+      }
+
+      console.log("onBlur==>", value);
+    },
+  },
+};
+</script>
--- src/pages/address/add/index.vue
+++ src/pages/address/add/index.vue
@@ -2,21 +2,74 @@
  * @Author: 一个为高薪头秃的程序媴
  * @Date: 2021-03-18 21:04:27
  * @LastEditors: 一个为高薪头秃的程序猿
- * @LastEditTime: 2021-03-18 21:07:20
+ * @LastEditTime: 2021-03-19 10:56:19
  * @Description: 添加地址
 -->
 <template>
-  <view class="addAddress-container">添加地址</view>
+  <view class="addAddress-container">
+    <!-- <view class="title">添加地址</view> -->
+    <input-val
+      v-model="form.name"
+      :name="'收货人'"
+      :label="'收货人'"
+      :placeholder="'请输入收货人'"
+      :required="true"
+      :border="true"
+      @enterVal="enterVal"
+    />
+    <!-- <input-val
+      v-model="form.mobile"
+      :name="'手机号'"
+      :label="'手机号'"
+      :placeholder="'请输入手机号'"
+      :required="true"
+      :border="true"
+      :type="'number'"
+      :errorMessage="mobileMsg"
+      @enterVal="enterVal"
+    /> -->
+    <!-- TODO:省市区选择 -->
+    <!-- <van-area area-list="{{ areaList }}" value="110101" /> -->
+    <!-- <input-val
+      v-model="form.addressDetail"
+      :name="'详细地址'"
+      :label="'详细地址'"
+      :placeholder="'请输入详细地址'"
+      :required="true"
+      :border="true"
+      :errorMessage="addressDetailMsg"
+      @enterVal="enterVal"
+    /> -->
+  </view>
 </template>
 
 <script>
+import inputVal from "../../../components/input";
 export default {
+  components: { inputVal },
   data() {
-    return {};
+    return {
+      areaList: {},
+      form: {
+        name: "",
+        mobile: "",
+        address: "",
+        addressDetail: "",
+      },
+    };
   },
-  onLoad() {},
   //方法集合
-  methods: {},
+  methods: {
+    enterVal(type, e) {
+      if (type === "收货人") {
+        this.form.name = e;
+      } else if (type === "手机号码") {
+        this.form.mobile = e;
+      } else {
+        this.form.addressDetail = e;
+      }
+    },
+  },
 };
 </script>
 <style lang="less">
