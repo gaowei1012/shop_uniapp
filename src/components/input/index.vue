349c68e2e62cc8205bee5a6d7668b72f86883c7b
suxiaoxiao <37326714+gcindy@users.noreply.github.com>
2021-03-20 13:22:47 +0800

    change 添加地址布局

--- src/components/input/index.vue
+++ src/components/input/index.vue
@@ -2,115 +2,64 @@
  * @Author: 一个为高薪头秃的程序媴
  * @Date: 2021-03-18 21:58:58
  * @LastEditors: 一个为高薪头秃的程序猿
- * @LastEditTime: 2021-03-19 10:53:48
+ * @LastEditTime: 2021-03-20 13:21:30
  * @Description: 封装input
 -->
-
 <template>
-  <!-- 
-    name       在表单内提交时的标识符
-    label      输入框左侧文本	
-    value      当前输入的值	
-    type       可设置为任意原生类型, 如 number idcard textarea digit	
-    clickable  是否开启点击反馈	
-    clearable	 是否启用清除控件
-    readonly   是否只读	
-    disabled   是否禁用输入框	
-    border     是否显示内边框	
-    required   是否显示表单必填星号
-    focus      是否获取焦点	
-    center     是否使内容垂直居中	
-    password   是否是密码类型	
-    placeholder	输入框为空时占位符
-    placeholder-style	指定 placeholder 的样式	
-
-   -->
-  <van-field
-    :name="name"
-    :label="label"
-    :type="type"
-    :clearable="clearable"
-    :readonly="readonly"
-    :disabled="disabled"
-    :placeholder="placeholder"
-    :border="border"
-    :required="required"
-    :error-message="errorMessage"
-    @change="onChange"
-    @blur="onBlur"
-  />
+  <view class="input-container">
+    <form>
+      <view class="form-item" v-for="(item, index) in array" :key="index">
+        <view class="title">{{ item.name }}</view>
+        <view class="input">
+          <input
+            v-model="form[item.model]"
+            :type="item.inputType ? item.inputType : 'text'"
+            :placeholder="item.placeholder ? item.placeholder : ''"
+            :readonly="item.readonly ? item.readonly : false"
+            :disabled="item.disabled ? item.disabled : false"
+            placeholder-class="placeholderClass"
+            @input="changeInput"
+            @click="clickInput(item.name)"
+          />
+          <label class="msg color-ED2324">{{ errMsg }}</label>
+        </view>
+      </view>
+      <view class="form-item">
+        <slot />
+      </view>
+    </form>
+  </view>
 </template>
 
 <script>
 export default {
   props: {
-    name: {
-      type: String,
-      default: () => "",
+    array: {
+      type: Array,
+      default: () => [],
     },
-    label: {
-      type: String,
-      default: () => "",
+    form: {
+      type: Object,
+      default: () => {},
     },
-    value: {
-      type: String,
-      default: () => "",
-    },
-    type: {
-      type: String,
-      default: () => "text",
-    },
-    clearable: {
-      type: Boolean,
-      default: () => true,
-    },
-    readonly: {
-      type: Boolean,
-      default: () => false,
-    },
-    disabled: {
-      type: Boolean,
-      default: () => false,
-    },
-    placeholder: {
-      type: String,
-      default: () => "",
-    },
-    required: {
-      type: Boolean,
-      default: () => false,
-    },
-    border: {
-      type: Boolean,
-      default: () => false,
-    },
-    reg: {
-      type: String,
-      default: () => "",
-    },
-  },
-  data() {
-    return {
-      errorMessage: "",
-    };
   },
 
   //方法集合
   methods: {
-    onChange(e) {
-      const { detail } = e;
-      this.$emit("enterVal", this.name, detail);
+    clickInput(e) {
+      console.log("e==>", e);
+      this.$emit("clickInput", e);
     },
-    onBlur(e) {
+    changeInput(e) {
       const { value } = e.detail;
-      if (!value) {
-        this.errorMessage = `请输入${this.name}的信息`;
-      }else {
-        this.errorMessage = "";
-      }
-
-      console.log("onBlur==>", value);
+      this.$emit("changeInput", this.name, value);
     },
   },
 };
 </script>
+<style lang="less">
+.msg {
+  padding-top: 24rpx;
+  font-size: 24rpx;
+}
+</style>
--- src/pages/address/add/index.vue
+++ src/pages/address/add/index.vue
@@ -2,44 +2,24 @@
  * @Author: 一个为高薪头秃的程序媴
  * @Date: 2021-03-18 21:04:27
  * @LastEditors: 一个为高薪头秃的程序猿
- * @LastEditTime: 2021-03-19 10:56:19
+ * @LastEditTime: 2021-03-20 13:22:08
  * @Description: 添加地址
 -->
 <template>
   <view class="addAddress-container">
     <!-- <view class="title">添加地址</view> -->
     <input-val
-      v-model="form.name"
-      :name="'收货人'"
-      :label="'收货人'"
-      :placeholder="'请输入收货人'"
-      :required="true"
-      :border="true"
-      @enterVal="enterVal"
-    />
-    <!-- <input-val
-      v-model="form.mobile"
-      :name="'手机号'"
-      :label="'手机号'"
-      :placeholder="'请输入手机号'"
-      :required="true"
-      :border="true"
-      :type="'number'"
-      :errorMessage="mobileMsg"
-      @enterVal="enterVal"
-    /> -->
-    <!-- TODO:省市区选择 -->
-    <!-- <van-area area-list="{{ areaList }}" value="110101" /> -->
-    <!-- <input-val
-      v-model="form.addressDetail"
-      :name="'详细地址'"
-      :label="'详细地址'"
-      :placeholder="'请输入详细地址'"
-      :required="true"
-      :border="true"
-      :errorMessage="addressDetailMsg"
-      @enterVal="enterVal"
-    /> -->
+      :form="form"
+      :array="array"
+      @changeInput="enterVal"
+      @formSubmit="formSubmit"
+      @clickInput="clickInput"
+    >
+      <!-- TODO:省市区选择 -->
+      <!-- <van-area :area-list="areaList" value="110101" /> -->
+      <van-button type="primary" @click="submitForm">提交</van-button>
+      <van-button type="default" @click="resetForm">清空</van-button>
+    </input-val>
   </view>
 </template>
 
@@ -49,7 +29,54 @@ export default {
   components: { inputVal },
   data() {
     return {
-      areaList: {},
+      areaList: {
+        province_list: {
+          110000: "北京市",
+          120000: "天津市",
+        },
+        city_list: {
+          110100: "北京市",
+          110200: "县",
+          120100: "天津市",
+          120200: "县",
+        },
+        county_list: {
+          110101: "东城区",
+          110102: "西城区",
+          110105: "朝阳区",
+          110106: "丰台区",
+          120101: "和平区",
+          120102: "河东区",
+          120103: "河西区",
+          120104: "南开区",
+          120105: "河北区",
+        },
+      },
+
+      array: [
+        {
+          name: "收货人",
+          placeholder: "请输入收货人",
+          model: "name",
+        },
+        {
+          name: "手机号码",
+          placeholder: "请输入手机号码",
+          model: "mobile",
+        },
+        {
+          name: "所在地区",
+          placeholder: "请输入所在地区",
+          model: "address",
+          disabled: true,
+        },
+        {
+          name: "详细地址",
+          placeholder: "请输入详细地址",
+          model: "addressDetail",
+        },
+      ],
+
       form: {
         name: "",
         mobile: "",
@@ -65,13 +92,39 @@ export default {
         this.form.name = e;
       } else if (type === "手机号码") {
         this.form.mobile = e;
+      } else if (type === "所在地区") {
+        this.form.address = e;
       } else {
         this.form.addressDetail = e;
       }
     },
+    // 提交表单
+    submitForm() {
+      console.log("submitForm==>", this.form);
+    },
+    // 重置form表单
+    resetForm() {
+      this.form = {
+        name: "",
+        mobile: "",
+        address: "",
+        addressDetail: "",
+      };
+    },
+    // 点击input框
+    clickInput(e) {},
   },
 };
 </script>
 <style lang="less">
 @import url("./index.less");
+.form-item {
+  display: flex;
+  .title {
+    flex: 3;
+  }
+  .input {
+    flex: 7;
+  }
+}
 </style>
