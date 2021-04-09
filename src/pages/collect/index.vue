11d7ce264b3359fd523b6e221e2607f00a802788
苏小妍 <13666683140@163.com>
2021-04-09 16:26:15 +0800

    change 商品收藏checkbox样式

--- src/pages/collect/index.vue
+++ src/pages/collect/index.vue
@@ -2,15 +2,19 @@
  * @Author: 一个为高薪头秃的程序媴
  * @Date: 2021-03-21 23:17:07
  * @LastEditors: 一个为高薪头秃的程序猿
- * @LastEditTime: 2021-03-22 11:18:35
+ * @LastEditTime: 2021-04-09 16:25:36
  * @Description: 商品收藏
 -->
 <template>
   <view class="collect-container">
-    <van-checkbox-group :value="checked" @change="onChange">
+    <checkbox-group @change="onChange">
       <view class="content-box">
         <view class="box" v-for="item in array" :key="item.id">
-          <van-checkbox :name="item.id" class="checkbox" />
+          <checkbox
+            :checked="item.id === is_checked || checkedAll"
+            color="#F759AB"
+            class="checkbox"
+          />
           <image :src="item.img" mode="scaleToFill" />
           <view class="title">{{ item.title }}</view>
           <view class="name">{{ item.name }}</view>
@@ -21,14 +25,15 @@
           </view>
         </view>
       </view>
-    </van-checkbox-group>
+    </checkbox-group>
 
     <view class="edit-deal">
       <template v-if="is_edit === true">
         <view class="deal">
-          <van-checkbox :value="checkedAll" @change="allChecked"
-            >全选</van-checkbox
-          >
+          <checkbox color="#F759AB" :checked="checkedAll" @click="allChecked">
+            全选
+          </checkbox>
+
           <view class="box">
             <view class="delete" @click="deleteShop">删除</view>
             <view class="finish" @click="is_edit = false">完成</view>
@@ -82,54 +87,47 @@ export default {
       ],
       is_edit: false,
       checkedAll: false,
-      checked: [],
+      is_checked: [],
     };
   },
-  //页面加载,上一个页面传值的options
-  onLoad(options) {},
-  //监听页面显示
-  onShow() {},
-  //监听页面初次渲染完成
-  onReady() {},
-  //监听页面隐藏
-  onHide() {},
-  //监听页面卸载
-  onUnload() {},
-  //方法集合
+  created() {
+    console.log(this.is_checked);
+  }, //方法集合
   methods: {
     // 单选
     onChange(event) {
-      const { detail } = event;
-      this.checked = detail;
-      this.checked.length === this.array.length
+      const { value } = event.detail;
+      this.is_checked = value;
+      this.is_checked.length === this.array.length
         ? (this.checkedAll = true)
         : (this.checkedAll = false);
     },
     // 全选
-    allChecked(event) {
-      this.checked = this.array.map((item) => {
-        return item.id.toString();
-      });
-      this.checkedAll = event.detail;
-      if (this.checkedAll === false) this.checked = [];
+    allChecked() {
+      this.checkedAll === false
+        ? (this.is_checked = [])
+        : (this.is_checked = this.array.map((item) => {
+            return item.id.toString();
+          }));
+      this.checkedAll = !this.checkedAll;
     },
     // 删除商品
     deleteShop() {
-      if (this.checked.length > 0) {
+      if (this.is_checked.length > 0) {
         // 删除所有商品
         if (
-          this.array.length == this.checked.length ||
+          this.array.length == this.is_checked.length ||
           this.checkedAll === true
         ) {
           this.array = [];
         } else {
-          const id = parseInt(this.checked.join());
+          const id = parseInt(this.is_checked.join());
           // 删除多个商品
           this.array.splice(
             this.array.findIndex((item) => item.id == id),
-            this.checked.length
+            this.is_checked.length
           );
-          this.checked = [];
+          this.is_checked = [];
         }
       } else {
       }
